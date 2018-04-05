e660d22dee510ae322cafce46aefc7e6cf4504ba
Sanchit Sinha <sinhasunny1979@gmail.com>
2018-04-05 15:34:01 -0700

    Corrected macros for emmitters and receivers

--- headers/macros.hpp
+++ headers/macros.hpp
@@ -5,15 +5,15 @@
 #define M2Reverse 5        // Right
 #define M2Forward 6
 
-#define Receiver_FL A9    //Facing left
-#define Receiver_L A8    //Facing forwards
-#define Receiver_R A7    //Facing forwards
-#define Receiver_FR A6    //Facing right
+#define Receiver_L 23    //Facing forwards
+#define Receiver_FL 22    //Facing left
+#define Receiver_FR 21    //Facing right
+#define Receiver_R 20    //Facing forwards
 
-#define Emitter_L 3        //Forwards
-#define Emitter_FL 6    //Left
-#define Emitter_FR 5        //Right
-#define Emitter_R 4        //Forwards
+#define Emitter_L 1        //Forwards
+#define Emitter_FL 3    //Left
+#define Emitter_FR 4        //Right
+#define Emitter_R 2        //Forwards
 
 #define EncoderL_A 23
 #define EncoderL_B 22
