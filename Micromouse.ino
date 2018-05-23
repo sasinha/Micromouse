c7a6ab4964b945e2e1417c184efd7af3027c25cb
Sanchit Sinha <sanchits@uci.edu>
2018-05-23 01:27:34 -0700

    Simplified ino

---
+++ Micromouse.ino
@@ -0,0 +1,16 @@
+
+# include "headers/main.hpp"
+
+void setup() {
+  // put your setup code here, to run once:
+  setupMM();
+  
+}
+
+//int done = 0;
+void loop() {
+  // put your main code here, to run repeatedly:
+  loopMM();
+
+
+}
