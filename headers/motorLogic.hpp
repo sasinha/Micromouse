96cee26a7d01cb8fb4f0cefa84d5740e6885793c
Sanchit Sinha <sanchits@uci.edu>
2018-05-22 23:26:53 -0700

    Motor logic readded

---
+++ headers/motorLogic.hpp
@@ -0,0 +1,14 @@
+// Motor setup
+
+# pragma once
+
+# include "macros.hpp"
+
+extern int motorSpeedLeft;
+extern int motorSpeedRight;
+
+void motorSetup(void);
+
+void motorRunLoop(void);
+
+void setStraight(void);
---
+++ src/motorLogic.cpp
@@ -0,0 +1,36 @@
+# include "../headers/motorLogic.hpp"
+
+# include <Arduino.h>
+
+
+int motorSpeedLeft = 0;
+int motorSpeedRight = 0;
+
+void motorSetup(void) {
+  // Motor speed pins
+  pinMode(M1Forward, OUTPUT);
+  pinMode(M1Reverse, OUTPUT);
+  pinMode(M2Forward, OUTPUT);
+  pinMode(M2Reverse, OUTPUT);
+
+}
+
+
+void motorRunLoop(void) {
+  if (motorSpeedLeft > 0) {
+    analogWrite(M2Forward, motorSpeedLeft % maxAnalogWrite);
+    analogWrite(M2Reverse, 0);
+  } else {
+    analogWrite(M2Reverse, (-1 * motorSpeedLeft) % maxAnalogWrite);
+    analogWrite(M2Forward, 0);
+  }
+
+  if (motorSpeedRight > 0) {
+    analogWrite(M1Forward, motorSpeedRight % maxAnalogWrite);
+    analogWrite(M1Reverse, 0);
+  } else {
+    analogWrite(M1Reverse, (-1 * motorSpeedRight) % maxAnalogWrite);
+    analogWrite(M1Forward, 0);
+  }
+
+}
